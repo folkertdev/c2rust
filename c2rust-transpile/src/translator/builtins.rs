698798daedf10a62f4e522a2e59c30956f107ba0
Folkert de Vries <folkert@folkertdev.nl>
2025-06-27 21:29:40 +0200

    translate `__builtin_assume` as `core::hint::assert_unchecked`

--- c2rust-transpile/src/translator/builtins.rs
+++ c2rust-transpile/src/translator/builtins.rs
@@ -659,6 +659,22 @@ impl<'c> Translation<'c> {
             // There's currently no way to replicate this functionality in Rust, so we just
             // pass the ptr input param in its place.
             "__builtin_assume_aligned" => Ok(self.convert_expr(ctx.used(), args[0])?),
+
+            //
+            "__builtin_assume" => {
+                let assert_unchecked_func =
+                    mk().abs_path_expr(vec!["core", "hint", "assert_unchecked"]);
+                let arg0 = self.convert_expr(ctx.used(), args[0])?;
+                arg0.and_then(|arg0| {
+                    let call_expr = mk().call_expr(assert_unchecked_func, vec![arg0]);
+                    self.convert_side_effects_expr(
+                        ctx,
+                        WithStmts::new_val(call_expr),
+                        "Builtin is not supposed to be used",
+                    )
+                })
+            }
+
             // Skip over, there's no way to implement it in Rust
             "__builtin_unwind_init" => Ok(WithStmts::new_val(self.panic_or_err("no value"))),
             "__builtin_unreachable" => Ok(WithStmts::new(
